42fd6fe275e8b55fcf5d7eaf8b90c6f00ba9a1b4
Colour1205 <Ccccchuck1@gmail.com>
2025-06-08 22:45:40 -0400

    update

--- scripts/main.js
+++ scripts/main.js
@@ -40,3 +40,53 @@ document.querySelectorAll('.modal-close-button').forEach(closeButton => {
     modalContainer.classList.remove('show');
   });
 });
+
+/* --- outer container background scroll logic --- */
+const outer_container = document.querySelector('.outer-container');
+const header_section = document.querySelector('.header-section');
+const blur_overshoot = document.querySelectorAll('.blur-overshoot');
+const buttons = document.querySelectorAll('.button');
+const social_section = document.querySelector('.social-section');
+const scroll_up_text = document.getElementById('scroll-up-text');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    header_section.classList.add('scrolled');
+    outer_container.classList.add('scrolled');
+    social_section.classList.add('scrolled');
+    scroll_up_text.classList.add('scrolled');
+
+    buttons.forEach(e1 => {
+      e1.classList.add('scrolled');
+    });
+
+    blur_overshoot.forEach(el => {
+      el.classList.add('animate-in');
+      el.classList.remove('animate-out');
+    });
+
+    document.documentElement.style.setProperty('--font-color', 'black');
+    document.documentElement.style.setProperty('--font-color-alt', 'white');
+    document.documentElement.style.setProperty('--button-background', 'rgba(0, 0, 0, 0.76)');
+    document.documentElement.style.setProperty('--font-weight', 'bold');
+  } else {
+    header_section.classList.remove('scrolled');
+    outer_container.classList.remove('scrolled');
+    social_section.classList.remove('scrolled');
+    scroll_up_text.classList.remove('scrolled');
+
+    buttons.forEach(e1 => {
+      e1.classList.remove('scrolled');
+    });
+
+    blur_overshoot.forEach(el => {
+      el.classList.remove('animate-in');
+      el.classList.add('animate-out');
+    });
+
+    document.documentElement.style.setProperty('--font-color', 'white');
+    document.documentElement.style.setProperty('--font-color-alt', 'black');
+    document.documentElement.style.setProperty('--button-background', 'rgba(255, 255, 255, 0.6)');
+    document.documentElement.style.setProperty('--font-weight', 'normal');
+  }
+});
